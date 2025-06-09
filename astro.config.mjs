13837d2a67c83d1b472bc0105ccea9e30095f438
Karol Moroz <karol@moroz.dev>
2025-06-10 03:16:16 +0800

    Only set Astro settings in production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+const isProd = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://moroz.github.io",
-  base: "megane",
+  // Add these two lines; replace `moroz` with your username
+  site: isProd ? "https://moroz.github.io" : undefined,
+  // Replace `megane` with the name of your project
+  base: isProd ? "megane" : undefined,
 });
